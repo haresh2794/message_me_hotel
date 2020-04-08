0cf65f9ca57d0535da0f6f5a9013e09db80cf57f
Ubuntu <ubuntu@ip-172-31-21-249.ap-southeast-1.compute.internal>
2020-04-08 11:27:59 +0000

    Chap 211 Authenciation system added

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,11 +4,22 @@ class SessionsController < ApplicationController
    end
    
    def create
+     
+     user = User.find_by(username: params[:session][:username])
+     if user && user.authenticate(params[:session][:password])
+       session[:user_id] = user.id
+       flash[:success] = "Login Sucessfull"
+       redirect_to root_path
+     else
+       flash.now[:error] = "Credentials error"
        render 'new'
+      end
    end
 
    def destroy
-       
+       session[:user_id] = nil
+       flash[:success] = "Logged out Successfully"
+       redirect_to login_path
    end
 
 end
