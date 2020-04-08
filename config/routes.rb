6fb76f0ff9540c45490f00fd1f6ad87739f8cc98
Ubuntu <ubuntu@ip-172-31-21-249.ap-southeast-1.compute.internal>
2020-04-08 14:53:22 +0000

    Chap 213 Messages from UI

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+ 
     root  'chatroom#index'
     
     get 'signup', to: 'users#new'
@@ -8,5 +9,7 @@ Rails.application.routes.draw do
     post 'login', to: 'sessions#create'
 
     delete 'logout', to: 'sessions#destroy'
+    
+    post 'message', to: 'messages#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
